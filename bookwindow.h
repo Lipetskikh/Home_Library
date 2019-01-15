d66d70402a216d49594ee582a6b1d33e30476d20
Lipetskikh <NastyaLAA28@gmail.com>
2019-01-16 00:29:42 +0300

    Create bookwindow.h

---
+++ bookwindow.h
@@ -0,0 +1,23 @@
+#ifndef BOOKWINDOW_H
+#define BOOKWINDOW_H
+
+#include <QtWidgets>
+#include <QtSql>
+
+#include "ui_bookwindow.h"
+
+
+class BookWindow: public QMainWindow
+{
+    Q_OBJECT
+public:
+    BookWindow();
+
+private:
+    void showError(const QSqlError &err);
+    Ui::BookWindow ui;
+    QSqlRelationalTableModel *model;
+    int authorIdx, genreIdx;
+};
+
+#endif
